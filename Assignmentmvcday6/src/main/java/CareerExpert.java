a17b646f1fe14ecdc338bbec11b93ab2afe54698
Amarnath2813 <72410948+Amarnath2813@users.noreply.github.com>
2023-12-08 09:09:29 +0530

    Add files via upload

---
+++ Assignmentmvcday6/src/main/java/CareerExpert.java
@@ -0,0 +1,11 @@
+
+public class CareerExpert {
+public String getAdvice(String Qualification) {
+	if(Qualification.contains("BE")) {
+		return "Zindagi me kuch bhi krna ye mat krna";
+	}
+	else {
+		return "Zindagi ke maje le le bhai";
+	}
+}
+}
---
+++ Assignmentmvcday6/src/main/java/CareerServ1.java
@@ -0,0 +1,28 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class CareerServ1
+ */
+public class CareerServ1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String s=request.getParameter("Qualification");
+		CareerExpert c = new CareerExpert();
+		String advice=c.getAdvice(s);
+		request.setAttribute("advice", advice);
+		request.getRequestDispatcher("/CareerDetails.jsp").forward(request, response);
+		
+	}
+
+}
