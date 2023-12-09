b46e56eb8f8cdd2c550178d71c4f6aabce9931f8
Amarnath <72410948+Amarnath2813@users.noreply.github.com>
2023-12-09 22:35:28 +0530

    Add files via upload

---
+++ UsebeanD7/src/main/java/mybean/MyClass2.java
@@ -0,0 +1,33 @@
+package mybean;
+public class MyClass2
+{
+	private String name;
+	private int age;
+	private String occupation;
+
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setAge(int age)
+	{
+		this.age=age;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public void setOccupation(String occupation)
+	{
+		this.occupation=occupation;
+	}
+	public String getOccupation()
+	{
+		return occupation;
+	}
+}
