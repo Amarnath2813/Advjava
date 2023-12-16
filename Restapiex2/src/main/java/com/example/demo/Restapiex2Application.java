4bff1acd42f412b32490003bd01a53fc2fb69460
Amarnath <72410948+Amarnath2813@users.noreply.github.com>
2023-12-16 12:21:11 +0530

    Add files via upload

---
+++ Restapiex2/src/main/java/com/example/demo/Restapiex2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restapiex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restapiex2Application.class, args);
+	}
+
+}
